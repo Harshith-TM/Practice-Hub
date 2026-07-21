09051de16acccc80185aebcf9ac2cc99b1801468
Harshith TM <tmharshith@gmail.com>
2026-07-21 13:32:45 +0530

    Add: SortingOrder class with ascending and descending order methods

--- Java/Practice/SortArray.java
+++ Java/Practice/SortArray.java
@@ -1,4 +1,4 @@
-//Java Program to sort an array in ascending order (without using built-in methods)
+//Java Program to sort an array (without using built-in methods)
 
 import java.util.Arrays;
 
@@ -8,8 +8,19 @@ public static void main(String args[])
 {
 
 int[] array = {-23,14,0,7,-9,31,-16,5,18,-2,27,-11,42,10,-35};
-int arraySize = array.length;
+SortingOrder sortingOrder = new SortingOrder();
+sortingOrder.ascendingOrder(array);
+sortingOrder.descendingOrder(array);
 
+}
+}
+
+class SortingOrder
+{
+
+void ascendingOrder(int[] array)
+{
+int arraySize = array.length;
 for(int i = 0;i < arraySize; i++)
 {
 for(int j = i+1;j < arraySize; j++)
@@ -22,8 +33,27 @@ array[j] = temp;
 }
 }
 }
-System.out.println("Sorted Array: ");
+System.out.println("Array sorted in ascending order: ");
 System.out.println(Arrays.toString(array));
-
 }
+
+void descendingOrder(int[] array)
+{
+int arraySize = array.length;
+for(int i = 0;i < arraySize; i++)
+{
+for(int j = i+1;j < arraySize; j++)
+{
+if(array[i]<array[j])
+{
+int temp = array[i]; 
+array[i] = array[j];
+array[j] = temp;
+}
+}
+}
+System.out.println("Array sorted in descending order: ");
+System.out.println(Arrays.toString(array));
+}
+
 }
